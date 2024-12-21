bd6745821a02a44190a08ec00a434867841b07e8
Nate Garcia <nategarcia.va@gmail.com>
2024-12-20 22:23:22 -0500

    Draws player

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     dt = 0
 
     while True:
@@ -12,6 +14,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit fps
